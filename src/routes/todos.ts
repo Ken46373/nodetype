aa3a0ee31bc95faf1c507e3c30bbcd7fab3c4615
Ken Minami <kenminami.dev@gmail.com>
2021-01-17 20:47:34 +0900

    controller作成

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
+import { createTodo } from '../controllers/todos';
 
 const router = Router();
 
-router.post("/");
+router.post("/", createTodo);
 
 router.get("/");
 
-router.patch("/");
+router.patch("/:id");
 
-router.delete("/");
+router.delete(":id");
 
 export default router;
