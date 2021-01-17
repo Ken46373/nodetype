ffb8d4ef2bfa248ffe7e57d55ff1005289f67335
Ken Minami <kenminami.dev@gmail.com>
2021-01-17 21:11:00 +0900

    CRUD処理追加

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 exports.createTodo = (req, res, next) => {
@@ -10,4 +10,26 @@ exports.createTodo = (req, res, next) => {
     res
         .status(201)
         .json({ message: "TODOを作成しました。", createdTodo: newTodo });
+};
+exports.getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updateText = req.body.text;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のTODOが見つかりませんでした。");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(todoId, updateText);
+    res.json({ message: "更新しました。", updatedTodo: TODOS[todoIndex] });
+};
+exports.deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のTODOが見つかりませんでした。");
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: "TODOを削除しました。" });
 };
